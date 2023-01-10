b3116fcceba5e7c6aebefffc7f27e2e490e1e2ee
tchisama <115560200+tchi-sama@users.noreply.github.com>
2023-01-10 01:52:20 -0800

    add the remove in the card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,7 +7,7 @@ import { useContext } from 'react';
 import { CartContext } from '../context/cartContext';
 import { AddToCart } from '../context/addToCart';
 import { motion } from "framer-motion"
-
+import RemoveRoundedIcon from '@mui/icons-material/RemoveRounded';
 import Skeleton from '@mui/material/Skeleton';
 import Stack from '@mui/material/Stack';
 
@@ -22,11 +22,11 @@ function Card({ img, title, price, description, productId }) {
         setSkeliton(true)
         setTimeout(() => {
             setSkeliton(false)
-        }, 500);
+        }, 800);
     }, [productId])
 
     const addtocartHandler = () => {
-        setAddToCart(pre=>pre+1)
+        setAddToCart(pre => pre + 1)
         let newCart = cart;
         let alreadyAdded = false;
         for (let i = 0; i < cart.length; i++) {
@@ -43,11 +43,52 @@ function Card({ img, title, price, description, productId }) {
                 }, ...a]
             );
         }
+        setQ(pre => pre + 1)
     }
+
+    const getProduct = cart.filter(item => {
+        return item.productId == productId
+    })
+
+    const [q, setQ] = useState(0);
+    useEffect(() => {
+        if (getProduct.length != 0) {
+            setQ(getProduct[0].q)
+        } else {
+            setQ(0)
+        }
+    }, [])
+    useEffect(() => {
+        if (q===0) {
+            setCart(cart.filter((item) => {
+                return item.productId != productId
+            }))
+        } 
+    }, [q])
+    
+
+    const removefromcartHandler = () => {
+        console.log(q)
+        let newCart = cart;
+        for (let i = 0; i < cart.length; i++) {
+            if (cart[i].productId == productId) {
+                newCart[i].q -= 1;
+                setCart(newCart);
+            }
+        }
+        setQ(pre => pre - 1)
+        setAddToCart(pre => pre + 1)
+    }
+
     return (
         !skeliton ?
-            <motion.div className='mx-auto card flex flex-col' >
-
+            <motion.div className='relative mx-auto card flex flex-col' >
+                {
+                    q !== 0 &&
+                    <div className='z-10 absolute bg-prime right-3 top-3 w-6 h-6 rounded-full flex justify-center items-center text-white'>
+                        {q}
+                    </div>
+                }
                 <Link to={"/market/product/" + productId}>
                     <div className='flex-1 flex items-center justify-center overflow-hidden'>
                         <img className='h-[150px] md:h-[200px] object-cover' src={img} alt="" />
@@ -57,18 +98,23 @@ function Card({ img, title, price, description, productId }) {
                     <Link to={"/market/product/" + productId}>
                         <h3 className='text-xs md:text-base font-medium text-gray-700'>{title}</h3>
                         <p className='text-xs md:text-xs text-gray-600 flex-1'>{description ? description : "no description"}</p>
-                    </Link>
-                    <div className='flex items-end justify-between mt-1 '>
                         <h2 className='text-base md:text-xl font-medium text-gray-700 w-full'>{price} DH</h2>
+                    </Link>
+                    <div className={'flex items-end mt-1 pt-2 ' + (q != 0 ? 'justify-between' : "justify-end")}>
                         {/* <Tooltip title="add to cart" arrow > */}
+                        {
+                            q !== 0 &&
+                            <button onClick={() => removefromcartHandler()} className='button p-1 md:p-2 cardBtn drop-shadow-md hover:bg-red-500 bg-red-400 text-white rounded-xl '>
+                                <RemoveRoundedIcon />
+                            </button>
+                        }
                         <button onClick={() => addtocartHandler()} className='button p-1 md:p-2 cardBtn drop-shadow-md hover:bg-[#85a864] bg-[#95BF6D] text-white rounded-xl'>
-                            {
-                                added ? <DoneRoundedIcon /> : <AddIcon />
-                            }
+                            <AddIcon />
                         </button>
                         {/* </Tooltip> */}
                     </div>
                 </div>
+                {/* <img className='absolute w-10' src={img} alt="" /> */}
             </motion.div>
             :
             <>
